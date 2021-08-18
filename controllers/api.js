13b4401fb3fd820baeb30735959c77db69a0264e
Casey R <crowlandsdev@gmail.com>
2021-08-18 16:49:26 -0400

    finished the routes and the built out he workout model

--- controllers/api.js
+++ controllers/api.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("/workouts", (req, res) => {
-    // send last workout data
+// route used to GET all workouts that have been done
+router.get("/workouts", (req, res) => {
   Workout.aggregate([
     {
       $addFields: {
@@ -20,9 +20,29 @@ router.post("/workouts", (req, res) => {
     });
 });
 
+// route to GET all workouts from the past 7 days 
+router.get("/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// route to POST a new workout you have done 
 router.post("/workouts", ({ body }, res) => {
-  // add new workout data
-  // const workout = new db.Workout(body);
   Workout.create(body)
     .then((dbWorkout) => {
       res.json(dbWorkout);
@@ -32,8 +52,22 @@ router.post("/workouts", ({ body }, res) => {
     });
 });
 
-router.put("/workouts/:id", ({body, params}, res) => {
-
+// route to UPDATE a past one of the workouts you have done 
+router.put("/workouts/:id", ({ body, params }, res) => {
+  const workoutRoutine = body;
+  Workout.findByIdAndUpdate(
+    { 
+      _id: `${params.id}` 
+    },
+    { 
+      $push: { exercises: workoutRoutine } 
+    })
+  .then((dbWorkout) => {
+    res.json(dbWorkout);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
--- models/workout.js
+++ models/workout.js
@@ -2,11 +2,34 @@ const mongoose = require("mongoose");
 
 const workoutSchema = new workoutSchema({
     day: {
-
+        type: Date,
+        default: Date.now
     },
-    excercises: {
-
-    }
+    exercises: [
+        {
+          type: {
+            type: String,
+            required: true,
+            trim: true
+          },
+          name: {
+            type: String,
+            required: true,
+            trim: true
+          },
+          duration: {
+            type: Number,
+            required: true,
+            trim: true
+          },
+          weight: {
+            type: Number
+          },
+          reps: Number,
+          sets: Number,
+          distance: Number,
+        },
+      ]
 })
 
 const Workout = mongoose.model("Workout", workout);
